118a42652c2c5edc5300ef226382f0079cd159d9
Angular_Developer <devloper.angular@gmail.com>
2023-01-31 12:20:40 +0530

    Add existing project files to Git

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Title check....
+  title = 'Angular_Training_Batch_One';
+}
